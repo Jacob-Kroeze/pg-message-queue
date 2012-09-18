75912a7e80563ac4f3e327bf9abbe05efaf61d2d
chris <chris@metatrontech.com>
2012-09-18 12:56:02 +0000

    Cleaning up implementation Getting rid of inheritance for queue tables Adding documentation Streamlining API to bytea and text returns of queus

--- pg_message_queue--0.1.sql
+++ pg_message_queue--0.1.sql
@@ -1,11 +1,17 @@
-\echo Use "CREATE EXTENSION pair" to load this file. \quit
+\echo Use "CREATE EXTENSION pg_message_queue" to load this file. \quit
 
-CREATE TABLE pg_mq_queue_catalog (
+CREATE TABLE pg_mq_config_catalog (
     table_name name not null unique,
     channel name primary key,
     payload_type text not null,
     check (payload_type IN ('text', 'xml', 'bytea'))
 );
+
+
+SELECT pg_catalog.pg_extension_config_dump('pg_mq_config_catalog', '');
+
+COMMENT ON TABLE pg_mq_config_catalog IS 
+$$ The base message queue catalog.  $$;
 
 CREATE TABLE pg_mq_base (
     msg_id bigserial not null,
@@ -35,7 +41,7 @@ LANGUAGE PLPGSQL AS
 $$
 DECLARE t_channel name;
 BEGIN
-   SELECT channel INTO t_channel FROM pg_mq_queue_catalog
+   SELECT channel INTO t_channel FROM pg_mq_config_catalog 
     WHERE table_name = TG_RELNAME;
 
    EXECUTE 'NOTIFY ' || quote_ident(t_channel);
@@ -44,25 +50,25 @@ END;
 $$;
 
 CREATE OR REPLACE FUNCTION pg_mq_create_queue(in_channel text, in_type text)
-RETURNS pg_mq_queue_catalog
+RETURNS pg_mq_config_catalog 
 LANGUAGE PLPGSQL VOLATILE SECURITY DEFINER AS $$
 
 DECLARE 
-    out_val pg_mq_queue_catalog%ROWTYPE;
+    out_val pg_mq_config_catalog%ROWTYPE;
     t_table_name name;
 BEGIN
 
 t_table_name := 'pg_mq_queue_' || in_channel;
 
-INSERT INTO pg_mq_queue_catalog (table_name, channel, payload_type)
+INSERT INTO pg_mq_config_catalog (table_name, channel, payload_type)
 VALUES (t_table_name, in_channel, in_type);
 
-SELECT * INTO out_val FROM pg_mq_queue_catalog 
+SELECT * INTO out_val FROM pg_mq_config_catalog 
  WHERE channel = in_channel;
 
 EXECUTE 'CREATE TABLE ' || quote_ident(t_table_name) || '(
-    PRIMARY KEY (msg_id)
-  ) INHERITS (' || quote_ident('pg_mq_' || in_type ) || ')';
+    like ' ||  quote_ident('pg_mq_' || in_type ) || ' INCLUDING ALL
+  )';
 
 EXECUTE 'CREATE TRIGGER pg_mq_notify
          AFTER INSERT ON ' || quote_ident(t_table_name) || '
@@ -82,12 +88,12 @@ declare t_table_name name;
 
 BEGIN
 
-   SELECT table_name INTO t_table_name FROM pg_mq_queue_catalog
+   SELECT table_name INTO t_table_name FROM pg_mq_config_catalog 
     WHERE channel = in_channel;
 
    EXECUTE 'DROP TABLE ' || quote_ident(t_table_name) || ' CASCADE';
 
-   DELETE FROM pg_mq_queue_catalog WHERE channel = in_channel;
+   DELETE FROM pg_mq_config_catalog WHERE channel = in_channel;
 
    RETURN FOUND;
 
@@ -100,10 +106,10 @@ CREATE OR REPLACE FUNCTION pg_mq_send_message
 (in_channel text, in_payload anyelement)
 RETURNS pg_mq_base
 LANGUAGE PLPGSQL VOLATILE AS $$
-    DECLARE cat_entry pg_mq_queue_catalog%ROWTYPE;
+    DECLARE cat_entry pg_mq_config_catalog%ROWTYPE;
             out_val pg_mq_base%ROWTYPE;
     BEGIN
-       SELECT * INTO cat_entry FROM pg_mq_queue_catalog
+       SELECT * INTO cat_entry FROM pg_mq_config_catalog
         WHERE channel = in_channel;
 
        EXECUTE 'INSERT INTO ' || quote_ident(cat_entry.table_name)
@@ -121,11 +127,11 @@ $$;
 CREATE OR REPLACE FUNCTION pg_mq_get_msg_text(in_channel name, in_num_msgs int)
 RETURNS SETOF pg_mq_text
 LANGUAGE PLPGSQL VOLATILE AS $$
-   DECLARE cat_entry pg_mq_queue_catalog%ROWTYPE;
+   DECLARE cat_entry pg_mq_config_catalog%ROWTYPE;
    BEGIN
-      SELECT * INTO cat_entry FROM pg_mq_queue_catalog
+      SELECT * INTO cat_entry FROM pg_mq_config_catalog
         WHERE channel = in_channel;
-      IF cat_entry.payload_type <> 'text' THEN
+      IF cat_entry.payload_type = 'bytea' THEN
          RAISE EXCEPTION 'Incorrect Type Called';
       END IF;
       RETURN QUERY EXECUTE
@@ -136,20 +142,17 @@ LANGUAGE PLPGSQL VOLATILE AS $$
                          $e$    WHERE was_delivered is not true
                              ORDER BY msg_id LIMIT $e$ || in_num_msgs || $e$
                              )
-          RETURNING * $e$;
+          RETURNING msg_id, sent_by, was_delivered, payload::text $e$;
 END;
 $$;
 
 CREATE OR REPLACE FUNCTION pg_mq_get_msg_bin(in_channel name, in_num_msgs int)
 RETURNS SETOF pg_mq_bytea 
 LANGUAGE PLPGSQL AS $$
-   DECLARE cat_entry pg_mq_queue_catalog%ROWTYPE;
+   DECLARE cat_entry pg_mq_config_catalog%ROWTYPE;
    BEGIN
-      SELECT * INTO cat_entry FROM pg_mq_queue_catalog
+      SELECT * INTO cat_entry FROM pg_mq_config_catalog
         WHERE channel = in_channel;
-      IF cat_entry.payload_type <> 'bytea' THEN
-         RAISE EXCEPTION 'Incorrect Type Called';
-      END IF;
       RETURN QUERY EXECUTE
          $e$ UPDATE $e$ || quote_ident(cat_entry.table_name) || $e$
                 SET was_delivered = true 
@@ -158,29 +161,7 @@ LANGUAGE PLPGSQL AS $$
                          $e$    WHERE was_delivered is not true
                              ORDER BY msg_id LIMIT $e$ || in_num_msgs || $e$
                              ) 
-          RETURNING * $e$;
-END;
-$$;
-
-CREATE OR REPLACE FUNCTION pg_mq_get_msg_xml(in_channel name, in_num_msgs int)
-RETURNS SETOF pg_mq_xml
-LANGUAGE PLPGSQL AS $$
-   DECLARE cat_entry pg_mq_queue_catalog%ROWTYPE;
-   BEGIN
-      SELECT * INTO cat_entry FROM pg_mq_queue_catalog
-        WHERE channel = in_channel;
-      IF cat_entry.payload_type <> 'xml' THEN
-         RAISE EXCEPTION 'Incorrect Type Called';
-      END IF;
-      RETURN QUERY EXECUTE
-         $e$ UPDATE $e$ || quote_ident(cat_entry.table_name) || $e$
-                SET was_delivered = true 
-              WHERE msg_id IN (SELECT msg_id 
-                                 FROM $e$ || quote_ident(cat_entry.table_name) || 
-                         $e$    WHERE was_delivered is not true
-                             ORDER BY msg_id LIMIT $e$ || in_num_msgs || $e$
-                             ) 
-          RETURNING * $e$;
+          RETURNING msg_id, sent_by, was_delivered, payload::bytea $e$;
 END;
 $$;
 
